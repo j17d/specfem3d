b97bd907b63284631b71b4649904f021af01f4e1
nqdu <nqdu@foxmail.com>
2025-02-17 23:31:48 -0500

    unified api for couple_with_injection

--- src/specfem3D/iterate_time.F90
+++ src/specfem3D/iterate_time.F90
@@ -274,6 +274,8 @@
           call update_displ_Newmark()
         endif
 
+        if(COUPLE_WITH_INJECTION_TECHNIQUE) call fetch_injection_wavefield()
+
         ! computes acoustic domain (first)
         if (ACOUSTIC_SIMULATION) call compute_forces_acoustic_forward_calling()
         ! computes elastic domain
